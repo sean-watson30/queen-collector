9807510edebf36cb645c07b3b2bd1cdb19c67b7a
Sean Watson <sean_watson30@me.com>
2022-07-08 11:59:24 -0600

    additional adjustments

--- main_app/urls.py
+++ main_app/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
   path('category/create/', views.CategoryCreate.as_view(), name='category_create'),
   path('category/<int:pk>/update/', views.CategoryUpdate.as_view(), name='category_update'),
   path('category/<int:pk>/delete/', views.CategoryDelete.as_view(), name='category_delete'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Queen, Category, Photo
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Queen, Category, Photo
 from .forms import LipSyncsForm
 import uuid
 import boto3
@@ -31,10 +35,12 @@ def about(request):
 #   Queen('Bendela Creme', '6 and All Stars 3', 'terminaly optimistic', False)
 # ]
 
+@login_required
 def queens_index(request):
   queens = Queen.objects.all()
   return render(request, 'queens/index.html', { 'queens': queens })
 
+@login_required
 def queens_detail(request, queen_id):
   queen = Queen.objects.get(id=queen_id)
   not_walked = Category.objects.exclude(id__in = queen.category.all().values_list('id'))
@@ -44,6 +50,8 @@ def queens_detail(request, queen_id):
   })
   # need to change naming here?
 
+
+@login_required
 def add_lipsync(request, queen_id):
   form = LipSyncsForm(request.POST)
   if form.is_valid():
@@ -52,16 +60,21 @@ def add_lipsync(request, queen_id):
     new_lipsync.save()
   return redirect('detail', queen_id=queen_id)
 
+
 # ___________ Many to Many Associations ___________
+@login_required
 def assoc_category(request, queen_id, category_id):
   Queen.objects.get(id=queen_id).category.add(category_id)
   return redirect('detail', queen_id=queen_id)
 
+@login_required
 def assoc_category_delete(request, queen_id, category_id):
   Queen.objects.get(id=queen_id).category.remove(category_id)
   return redirect('detail', queen_id=queen_id)
-  
+
+
 # ___________ Photo Handling ___________  
+@login_required
 def add_photo(request, queen_id):
   photo_file = request.FILES.get('photo-file', None)
   if photo_file:
@@ -77,40 +90,61 @@ def add_photo(request, queen_id):
       return redirect('detail', queen_id=queen_id)
   return redirect('detail', queen_id=queen_id)
 
+
+# ___________ Sign Up Function ___________
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid information. Please try again.'
+  form = UserCreationForm()
+  context = { 'form': form, 'error_message': error_message }
+  return render(request, 'registration/signup.html', context)  
+
+
 # ___________ Class Declarations / Queen ___________
-class QueenCreate(CreateView):
+class QueenCreate(LoginRequiredMixin, CreateView):
   model = Queen
   fields = '__all__'
   success_url = '/queens/'
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
-class QueenUpdate(UpdateView):
+class QueenUpdate(LoginRequiredMixin, UpdateView):
   model = Queen
   fields = ['season', 'description']
 # add 'winner' above?
 
-class QueenDelete(DeleteView):
+class QueenDelete(LoginRequiredMixin, DeleteView):
   model = Queen
   success_url = '/queens/'
 
+
 # ___________ Class Declarations / Category ___________
 
-class CategoryList(ListView):
+class CategoryList(LoginRequiredMixin, ListView):
   model = Category
   template_name = 'category/index.html'
 
-class CategoryDetail(DetailView):
+class CategoryDetail(LoginRequiredMixin, DetailView):
   model = Category
   template_name = 'category/detail.html'
 
-class CategoryCreate(CreateView):
+class CategoryCreate(LoginRequiredMixin, CreateView):
   model = Category
   fields = ['name']
   success_url = '/category/'
 
-class CategoryUpdate(UpdateView):
+class CategoryUpdate(LoginRequiredMixin, UpdateView):
   model = Category
   fields = ['name']
 
-class CategoryDelete(DeleteView):
+class CategoryDelete(LoginRequiredMixin, DeleteView):
   model = Category
   success_url = '/category/'
